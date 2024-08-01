3f548c915961f7e5f2c43b6991f4685e27dd76ac
Sakurathanh2003 <thanhvt@fit-haui.edu.vn>
2024-08-01 23:11:48 +0700

    update

--- BTL/resources/views/auth/passwords/confirm.blade.php
+++ BTL/resources/views/auth/passwords/confirm.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+                <div class="card-header">{{ __('Xác nhận mật khẩu') }}</div>
 
                 <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+                    {{ __('Vui lòng xác nhận mật khẩu trước khi tiếp tục.') }}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
                         @csrf
@@ -30,12 +30,12 @@
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
+                                    {{ __('Xác nhận mật khẩu') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Quên mật khẩu?') }}
                                     </a>
                                 @endif
                             </div>
